8d98e6c9c8be93b6158313b76369fb8eb3ec41ac
Bianca Reine Betos <120092488+biaacakes@users.noreply.github.com>
2023-03-03 01:34:35 +0800

    Add files via upload

---
+++ Snake/src/ColorMenu.java
@@ -0,0 +1,83 @@
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import java.beans.EventHandler;
+
+import java.awt.event.ActionEvent;
+
+public class ColorMenu extends JFrame {
+    JList list;
+    private String[] colorlist ={"BLACK" , "WHITE" , "PINK" , "MAGENTA" , "CYAN" , "YELLOW"};
+    private Color[] colors= {Color.BLACK, Color.WHITE, Color.PINK, Color.MAGENTA, Color.CYAN, Color.YELLOW};
+
+    ColorMenu () {
+        super("Snake Game");
+        this.setLayout(new FlowLayout());
+
+        ImageIcon icon= new ImageIcon("kulay.gif");
+        JLabel label1= new JLabel();
+        label1.setIcon(icon);
+
+
+    JLabel label = new JLabel();
+   
+    label.setText("Choose your preferred color of Snake");
+    label.setFont(new Font("Times New Roman", Font.BOLD, 10));
+    label.setIcon(icon);
+    
+    //JList Component
+    list = new JList(colorlist);
+    list.setVisibleRowCount(3);
+
+    //Sets Selection Mode
+    list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+    EventHandler handler = new EventHandler();
+    list.addListSelectionListener(handler);
+
+
+    JScrollPane scrollPane = new JScrollPane(list);
+
+
+
+    this.add(label1);
+    this.add(label);
+    this.add(scrollPane);
+    
+    
+        //Close java program when using window
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //Dimension of frame
+        this.setSize(285,285);
+        //Makes frame appear on screen
+        this.setVisible(true);
+        //Set frame to center
+        this.setLocationRelativeTo(null);
+        //Set background color of frame
+        this.getContentPane().setBackground(Color.white); 
+
+
+    }
+    private class EventHandler implements ListSelectionListener {
+
+        public void valueChanged(ListSelectionEvent event) {
+            //JOptionPane.showMessageDialog(null, "HELLO BIANCA!");
+            
+            GamePanel gp= new GamePanel();
+            gp.snakeColor= colors[list.getSelectedIndex()];
+
+            new PlayerMenu();
+            dispose(); 
+        }
+    
+}
+}
---
+++ Snake/src/MainMenu.java
@@ -0,0 +1,57 @@
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.border.Border;
+
+import java.awt.*;
+import java.beans.EventHandler;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class MainMenu extends JFrame  {
+    MainMenu() {
+        super("Snake Game");
+        this.setLayout(new FlowLayout());
+
+        //PICKING OF PHOTO FOR ICON
+        ImageIcon icon= new ImageIcon("nokia.gif");
+        //ICON APPEARANCE ON SCREEN
+        JLabel label= new JLabel();
+        label.setIcon(icon);
+        
+        //JButton
+        ImageIcon icon1 = new ImageIcon("nge.jpg");
+        JButton button= new JButton ();
+        button.setText("START GAME");
+        button.setFont(new Font("Consolas", Font.BOLD, 20));
+        button.setBackground(Color.WHITE);
+        button.setIcon(icon1);
+        button.setBounds(100, 100, 100, 100);
+        
+        //Add events to button
+        EventHandler handler= new EventHandler();
+        button.addActionListener(handler);
+
+        //Add components to fram
+        this.add(label);
+        this.add(button);
+
+    }
+    //EVENT HANDLER CLASS
+    private class EventHandler implements ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
+            //JOptionPane.showMessageDialog(null, "HELLO BIANCA!");
+
+            //SWITCHES TO GAME
+            new DifficultyMenu();
+
+            //Closes Main Menu
+            dispose();
+        }
+    }
+}
